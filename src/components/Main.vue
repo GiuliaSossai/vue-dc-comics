3e6ee4db9cd555887c1d89b4b3739434ad4e52b5
GiuliaSossai <giulia.sossai@yahoo.it>
2021-11-25 12:12:37 +0100

    ciclo for per cards

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,19 +1,60 @@
 <template>
   <main>
-     <div class="container">main</div>
+      <div class="jumbotron"></div>
+
+      <div class="container">
+         <div class="main-cards">
+            <MakeCard
+               v-for="(card, index) in CardsList"  
+               :key="index" 
+               :card="card"
+            />
+            
+         </div>
+      </div> 
    </main>
 </template>
 
 <script>
+
+import MakeCard from './MakeCard';
+import CardsList from '../assets/data/cards';
+
 export default {
-   name: 'Main'
+   name: 'Main',
+
+   components:{
+      MakeCard
+   },
+
+   data(){
+      return{
+         CardsList
+      }
+   }
 }
 </script>
 
 <style lang="scss">
    main{
-      background-color: #000;
-      color: #fff;
-      padding: 3rem;
+      background-color: #1C1C1C;
+      .jumbotron{
+         height: 300px;
+         overflow: hidden;
+         background-position: top;
+         background-image: url('../assets/img/jumbotron.jpg');
+         background-size: cover;
+      }
+
+      .container{
+         color: #fff;
+         padding: 3rem 0;
+         text-transform: uppercase;
+         .main-cards{
+            display: flex;
+            flex-wrap: wrap;
+         }
+      }
+      
    }
 </style>
---
+++ src/components/MakeCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card-box">
+     <img :src="card.thumb" :alt="card.series">
+     
+     <h5>{{card.series}}</h5>
+  </div>
+
+</template>
+
+<script>
+export default {
+   name: 'MakeCard',
+   props: {
+      card: Object
+   }
+}
+</script>
+
+<style lang="scss">
+   @import '../assets/style/mixins.scss';
+   @import '../assets/style/var.scss';
+
+   .card-box {
+      flex-basis: calc(100% / 6);
+      border: 1rem solid #1C1C1C;
+      img {
+         width: 100vw;
+         height: 188px;
+         object-fit: cover;
+         object-position: top;
+      }
+      h5 {
+         padding-top: 8px;
+      }
+   }
+</style>
